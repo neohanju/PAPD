171f41f2fd8cb04973109c983d79c38815d66a80
hellbell <oodgnas@gmail.com>
2015-10-14 16:52:53 +0900

    DPM results images +  modifying getPartCandidate
    Add test image's DPM results.
    Modify getPartCandidate.m

--- getPartCandidate.m
+++ getPartCandidate.m
@@ -79,47 +79,48 @@ addpath(genpath(DPM_path));
 % Get part candidates (root + 8 parts)
 %--------------------------------------------------------------------------
 % Load test image
-imageName = 'img4';
-im = imread(fullfile('data', sprintf('%s.jpg', imageName)));
-im = imresize(im, 2);
-% Load DPM model
-load('model\INRIAPERSON_star.mat');
-
-% Get feature pyramid
-pyra = featpyramid(double(im), model);
-
-% Get part candidates (root + 8 parts) 
-%       this contains part locations and scores
-[coords, partscores] = cascade_part_candidates(pyra, model, model.thresh);
-[sortO, sortI] = sort(model.cascade.order{1}(1:9));
-partscores = partscores([sortI 10],:);
-
-%===========================
-% Show detection results
-%===========================
-% draw head
-dets = coords([1:4 end-1 end],:)';
-over_head = 1;
-over_full = 0.5;
-I = nms2(dets, over_head);
-figure(1);
-imshow(im, 'border', 'tight');
-showboxes(im, coords(5:8,I)');
-title(sprintf('head, nms=%f',over_head));
-
-% draw full body
-figure(2);
-I = nms2(dets, over_full);
-imshow(im, 'border', 'tight');
-% showboxes(im, dets(I,:));
-showboxes(im, coords(1:4,I)');
-title(sprintf('full, nms=%f',over_full));
-
-
-% pause;
-% Save part candidates
-save(fullfile('data', sprintf('%s_part_candidates.mat',imageName)), 'coords', 'partscores');
-
-
-
+for imgIdx = 1 : 5
+    imageName = sprintf('img%d',imgIdx);
+    im = imread(fullfile('data', sprintf('%s.jpg', imageName)));
+    im = imresize(im, 2);
+    % Load DPM model
+    load('model\INRIAPERSON_star.mat');
+    
+    % Get feature pyramid
+    pyra = featpyramid(double(im), model);
+    
+    % Get part candidates (root + 8 parts)
+    %       this contains part locations and scores
+    [coords, partscores] = cascade_part_candidates(pyra, model, model.thresh);
+    [sortO, sortI] = sort(model.cascade.order{1}(1:9));
+    partscores = partscores([sortI 10],:);
+    
+    %===========================
+    % Show detection results
+    %===========================
+    % draw head
+    dets = coords([1:4 end-1 end],:)';
+    over_head = 1;
+    over_full = 0.5;
+    I = nms2(dets, over_head);
+    figure(1);
+    imshow(im, 'border', 'tight');
+    showboxes(im, coords(5:8,I)');
+    title(sprintf('head, nms=%f',over_head));
+    saveas(gcf, fullfile('data', sprintf('%s_DPM_head.jpg',imageName))); 
+    
+    % draw full body
+    figure(2);
+    I = nms2(dets, over_full);
+    imshow(im, 'border', 'tight');
+    % showboxes(im, dets(I,:));
+    showboxes(im, coords(1:4,I)');
+    title(sprintf('full, nms=%f',over_full));
+    saveas(gcf, fullfile('data', sprintf('%s_DPM_full_nms.jpg',imageName))); 
+    
+%     pause;
+    % Save part candidates
+%     save(fullfile('data', sprintf('%s_part_candidates.mat',imageName)), 'coords', 'partscores');
+    
+end
 
