01642eece7b2e5563c2893d64ee123abc62b44f1
neohanju <neohanju@gmail.com>
2015-11-05 20:42:36 +0900

    figure extraction temporary codes

--- ExtracFigures.m
+++ ExtracFigures.m
@@ -21,14 +21,23 @@
 
 % init
 main_init;
-TARGET_FRAME = 85;
-CROP_ZONE = [300 440 122 420];
-CROP_WIDTH = CROP_ZONE(2) - CROP_ZONE(1) + 1;
-CROP_HEIGHT = CROP_ZONE(4) - CROP_ZONE(3) + 1;
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% FOR OVERALL
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+TARGET_FRAME = 80;
+IMAGE_RESCALE = 1.5;
+CROP_CETNER = [627, 300];
+CROP_WIDTH = 130;
+CROP_HEIGHT = 260;
+CROP_ZONE = round([CROP_CETNER - [CROP_WIDTH/(2*IMAGE_RESCALE), CROP_HEIGHT/(2*IMAGE_RESCALE)], ...
+    CROP_CETNER + [CROP_WIDTH/(2*IMAGE_RESCALE)-1, CROP_HEIGHT/(2*IMAGE_RESCALE)-1]]);
 
 % load target frame
 image = imread(fullfile(IMAGE_DIR, sprintf([IMAGE_NAMEFORM, '.', IMAGE_FORMATE], TARGET_FRAME)));
-cropImage = image(CROP_ZONE(3):CROP_ZONE(4),CROP_ZONE(1):CROP_ZONE(2),:);
+cropImage = image(CROP_ZONE(2):CROP_ZONE(4),CROP_ZONE(1):CROP_ZONE(3),:);
+cropImage = imresize(cropImage, IMAGE_RESCALE);
 
 % load parts
 partPath = fullfile(PARTCANDIDATE_DIR, sprintf(PARTCANDIDATE_FORM, TARGET_FRAME));
@@ -53,18 +62,119 @@ end
 % draw parts
 for typeIdx = 1:numPartTypes
     figCurrent = figure(typeIdx); imshow(cropImage, 'border', 'tight'); hold on;
-    set(figCurrent, 'position', [typeIdx*(CROP_WIDTH+4)-CROP_WIDTH, 400, CROP_WIDTH, CROP_HEIGHT]);
+    set(figCurrent, 'position', [typeIdx*(CROP_WIDTH+10)-CROP_WIDTH, 400, CROP_WIDTH, CROP_HEIGHT]);
     for partIdx = 1:size(parts{typeIdx}, 1)
         x = parts{typeIdx}(partIdx,1);
         y = parts{typeIdx}(partIdx,2);
-        w = parts{typeIdx}(partIdx,3) - x + 1;
-        h = parts{typeIdx}(partIdx,4) - y + 1;
-        x = x - CROP_ZONE(1) + 1;
-        y = y - CROP_ZONE(3) + 1;
+        w = (parts{typeIdx}(partIdx,3) - x + 1) * IMAGE_RESCALE;
+        h = (parts{typeIdx}(partIdx,4) - y + 1) * IMAGE_RESCALE;
+        x = (x - CROP_ZONE(1) + 1) * IMAGE_RESCALE;
+        y = (y - CROP_ZONE(3) + 1) * IMAGE_RESCALE;
         rectangle('position', [x,y,w,h], 'EdgeColor', [0.7,0.7,0.7]);
     end
     hold off;
 end
+
+
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% % FOR RESULT
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% TARGET_FRAME = 80;
+% 
+% % load(fullfile(RESULT_DIR, 'result_hnr0.80_pnr0.50_SVM2GN.mat'));
+% image = imread(fullfile(IMAGE_DIR, sprintf([IMAGE_NAMEFORM, '.', IMAGE_FORMATE], TARGET_FRAME)));
+% 
+% partBoxes = curDetectionResult(1).cellPBoxs{TARGET_FRAME+1};
+% 
+% 
+% [numBoxes,~,numParts] = size(partBoxes);
+% figure(10);
+% imshow(image, 'border', 'tight'); hold on;
+% 
+% % frame information
+% % text(10, 20, ...
+% %     ['\fontsize{10}\color{white}' ...
+% %         sprintf('frame: %03d / detected boxes: %03d', TARGET_FRAME, numBoxes)], ...
+% %     'BackgroundColor', 'k');
+% 
+% % draw part boxes
+% for bIdx = 1:numBoxes
+%     curColor = GetColor(CDC, bIdx+7);
+%     bRoot = true;
+%     for pIdx = 1:numParts
+%         curPart = partBoxes(bIdx,1:4,pIdx);
+%         if 0 == curPart(3), continue; end
+%         if bRoot
+%             bRoot = false;
+%             continue;
+%         end
+%         xs = [curPart(1), curPart(1)+curPart(3), curPart(1)+curPart(3), curPart(1)];
+%         ys = [curPart(2), curPart(2), curPart(2)+curPart(4), curPart(2)+curPart(4)];
+%         p  = patch(xs, ys, curColor);
+%         set(p, 'FaceAlpha', 0.3);
+%         rectangle('Position', curPart, 'EdgeColor', curColor);
+%     end
+% end
+% hold off;
+% drawnow;
+
+
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %% RESULT VIDEO RECORDING
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% 
+% % load(fullfile(RESULT_DIR, 'result_hnr0.80_pnr0.50_SVM2GN.mat'));
+% writerObj = VideoWriter('result_video_S2L3.avi');
+% writerObj.FrameRate = 7;
+% open(writerObj);
+% 
+% % stDetectionResult = curDetectionResult(1);
+% stDetectionResult = interDetectionResult;
+% 
+% figFrameResult = figure;
+% for frameIdx = 0:181
+%     image = imread(fullfile('D:\Workspace\Dataset\PETS2009\S2\L3\Time_14-41\View_001'...
+%         , sprintf([IMAGE_NAMEFORM, '.', IMAGE_FORMATE], frameIdx)));
+% 
+%     partBoxes = stDetectionResult.cellPBoxs{frameIdx+1};
+% 
+% 
+%     [numBoxes,~,numParts] = size(partBoxes);
+%     figure(figFrameResult);
+%     imshow(image, 'border', 'tight'); hold on;
+% 
+%     % frame information
+%     text(10, 20, ...
+%         ['\fontsize{10}\color{white}' ...
+%             sprintf('frame: %03d / detected boxes: %03d', frameIdx, numBoxes)], ...
+%         'BackgroundColor', 'k');
+% 
+%     % draw part boxes
+%     for bIdx = 1:numBoxes
+%         curColor = GetColor(CDC, bIdx);
+%         bRoot = true;
+%         for pIdx = 1:numParts
+%             curPart = partBoxes(bIdx,1:4,pIdx);
+%             if 0 == curPart(3), continue; end
+%             if bRoot
+%                 bRoot = false;
+%                 continue;
+%             end
+%             xs = [curPart(1), curPart(1)+curPart(3), curPart(1)+curPart(3), curPart(1)];
+%             ys = [curPart(2), curPart(2), curPart(2)+curPart(4), curPart(2)+curPart(4)];
+%             p  = patch(xs, ys, curColor);
+%             set(p, 'FaceAlpha', 0.3);
+%             rectangle('Position', curPart, 'EdgeColor', curColor);
+%         end
+%     end
+%     hold off;
+%     drawnow;
+%     
+%     writeVideo(writerObj, im2frame(zbuffer_cdata(figFrameResult)));
+% end
+% 
+% close(writerObj);
 
 %()()
 %('') HAANJU.YOO
